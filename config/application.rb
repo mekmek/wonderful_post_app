2b9c7212de08c8f81ffa1d7920b28e9316fc86ad
mekmek <kawasaki.or4nek@gmail.com>
2022-03-11 22:19:29 +0900

    タイムゾーンをTokyoに設定

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module WonderfulPostApp
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.jbuilder false
