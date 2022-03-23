e0a1bb57ea0d1ef42973f2efc25abd99be2e9967
mekmek <kawasaki.or4nek@gmail.com>
2022-03-24 01:16:28 +0900

    seedファイルのリファクタリング

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,20 @@
-# テストユーザーの作成
-users = User.all.pluck(:email)
+# テストユーザーおよびユーザーに紐づく記事の作成
+existing_users = User.all.pluck(:email)
 password = "test1234"
 
 3.times do |num|
   email = "user00#{num + 1}@test.com"
-  unless users.include?(email)
-    User.create!(
+  unless existing_users.include?(email)
+    new_user = User.create!(
       email: email,
       password: password
     )
-  end
-end
 
-# 各ユーザーに紐付いた記事を50記事作成
-User.all.each do |user|
-  50.times do |num|
-    articles = user.articles.pluck(:title)
-    title = "No.#{num + 1}: user00#{user.id}の記事"
-    content = "No.#{num + 1}: user00#{user.id}の記事の本文"
+    50.times do |num|
+      title = "No.#{num + 1}: user00#{new_user.id}の記事"
+      content = "No.#{num + 1}: user00#{new_user.id}の記事の本文"
 
-    unless articles.include?(title)
-      user.articles.create!(
+      new_user.articles.create!(
         title: title,
         content: content
       )
