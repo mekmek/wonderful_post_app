d76a7e661677eb80ec48bfa9ef2e8ebc7e481673
mekmek <kawasaki.or4nek@gmail.com>
2022-03-24 01:04:05 +0900

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# テストユーザーの作成
+users = User.all.pluck(:email)
+password = "test1234"
+
+3.times do |num|
+  email = "user00#{num + 1}@test.com"
+  unless users.include?(email)
+    User.create!(
+      email: email,
+      password: password
+    )
+  end
+end
+
+# 各ユーザーに紐付いた記事を50記事作成
+User.all.each do |user|
+  50.times do |num|
+    articles = user.articles.pluck(:title)
+    title = "No.#{num + 1}: user00#{user.id}の記事"
+    content = "No.#{num + 1}: user00#{user.id}の記事の本文"
+
+    unless articles.include?(title)
+      user.articles.create!(
+        title: title,
+        content: content
+      )
+    end
+  end
+end
