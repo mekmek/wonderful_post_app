0057e96440969a20f9f9bf92c7daed66c4228c6a
mekmek <kawasaki.or4nek@gmail.com>
2022-03-30 21:06:29 +0900

    articleとtagを関連付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, :content, presence: true
   belongs_to :user
+
+  has_many :tag_articles, dependent: :destroy
+  has_many :tags, through: :tag_articles
 end
